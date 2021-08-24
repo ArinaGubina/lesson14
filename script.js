380f729f8ad63f0cf45fb40d517e2d2c5b7b4b96
Arina Gubina <fike-01@mail.ru>
2021-08-25 01:27:28 +0700

    Доп задание с квадратом

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 let square = {
     selector: '.red_square',
-    height: '140px',
-    width: '140px',
+    height: '100px',
+    width: '100px',
     bg: 'red',
-    fontSize: '16px'
+    fontSize: '16px',
 };
 
 let rectangle = {
@@ -14,6 +14,12 @@ let rectangle = {
     width: '240px',
     bg: 'orange',
     fontSize: '16px'
+};
+
+let squareOptions = {
+    position: 'absolute',
+    top: 'calc(50% - 50px)',
+    left: 'calc(50% - 50px)'
 };
 
 class DomElement {
@@ -25,7 +31,7 @@ class DomElement {
         this.fontSize = options.fontSize;
     }
     
-    create(text) {
+    create(text, addOptions) {
         let element;
         if (this.selector[0] === '.') {
             element = document.createElement('div');
@@ -37,15 +43,48 @@ class DomElement {
         }
         element.innerHTML = text;
         document.body.append(element);
-        element.style.cssText = 'background-color:' + this.bg + ';width:' + this.width +
+
+        let styles = 'background-color:' + this.bg + ';width:' + this.width +
             ';height: ' + this.height + ';font-size:' + this.fontSize +
             ';display:flex;justify-content:center;align-items:center;';
+         
+        if(addOptions) {              
+            this.eventArrow(function(arrow) {
+                if(arrow === 'ArrowUp') {
+                    let top = +window.getComputedStyle(element).top.slice(0, -2);
+                    element.style.top = (top - 10) + 'px';
+                }
+                if(arrow === 'ArrowDown') {
+                    let top = +window.getComputedStyle(element).top.slice(0, -2);
+                    element.style.top = (top + 10) + 'px';
+                }
+                if(arrow === 'ArrowLeft') {
+                    let left = +window.getComputedStyle(element).left.slice(0, -2);
+                    element.style.left = (left - 10) + 'px';
+                }
+                if(arrow === 'ArrowRight') {
+                    let left = +window.getComputedStyle(element).left.slice(0, -2);
+                    element.style.left = (left + 10) + 'px';
+                }
+            });
+        }
+        for (const key in addOptions) {
+            styles += key + ':' + addOptions[key] + ';';
+        }
+        element.style.cssText = styles;
+    }
+    eventArrow(callback) {
+        document.body.addEventListener('keydown', function (event) {
+            callback(event.code);
+        });
     }
 }
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    let dom = new DomElement(square);
+    dom.create('Это квадрат', squareOptions);
 
-let dom = new DomElement(square);
-dom.create('Это квадрат');
+    let dom2 = new DomElement(rectangle);
+    dom2.create('Это прямоугольник');
+});
 
-let dom2 = new DomElement(rectangle);
-dom2.create('Это прямоугольник');
